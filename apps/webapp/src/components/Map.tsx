28cd25ef90e26e8a78b5a3e1157511b3cc8f165b
Yekna <urosjeknic@gmail.com>
2023-02-26 14:04:25 +0100

    Use local storage to store coordinates from last visit to home page

--- apps/webapp/src/components/Map.tsx
+++ apps/webapp/src/components/Map.tsx
@@ -1,9 +1,9 @@
-import { useState } from "react";
+import { useLocalState } from "@diplomski/hooks/useLocalState";
 import ReactMapGl, { ViewState } from "react-map-gl";
 import SearchBox from "./SearchBox";
 
 export default function Map() {
-  const [viewport, setViewport] = useState<ViewState>({
+  const [viewport, setViewport] = useLocalState<ViewState>("viewport", {
     latitude: 46.09167269144208,
     longitude: 19.66244234405549,
     zoom: 10,
@@ -18,31 +18,29 @@ export default function Map() {
   });
 
   return (
-    <div className="text-black relative">
-      <ReactMapGl
-        {...viewport}
-        style={{ width: "100%", height: "100vh", cursor: "grab" }}
-        mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_API_TOKEN}
-        onMove={({ viewState }) => setViewport(viewState)}
-        minZoom={10}
-        maxZoom={15}
-        mapStyle="mapbox://styles/leighhalliday/ckhjaksxg0x2v19s1ovps41ef"
-      >
-        <div className="absolute top-0 w-full z-10 p-4">
-          <SearchBox
-            onSelectAddress={(_address: string, latitude: number, longitude: number) => {
-              if (latitude && longitude) {
-                setViewport((old) => ({
-                  ...old,
-                  latitude,
-                  longitude,
-                  zoom: 15
-                }));
-              }
-            }}
-          />
-        </div>
-      </ReactMapGl>
-    </div>
+    <ReactMapGl
+      {...viewport}
+      style={{ width: "100%", height: `calc(100vh - 64px)`, cursor: "grab" }}
+      mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_API_TOKEN}
+      onMove={({ viewState }) => setViewport(viewState)}
+      minZoom={10}
+      maxZoom={15}
+      mapStyle="mapbox://styles/leighhalliday/ckhjaksxg0x2v19s1ovps41ef"
+    >
+      <div className="absolute top-0 w-full z-10 p-4">
+        <SearchBox
+          onSelectAddress={(_address, latitude, longitude) => {
+            if (latitude && longitude) {
+              setViewport((old) => ({
+                ...old,
+                latitude,
+                longitude,
+                zoom: 15,
+              }));
+            }
+          }}
+        />
+      </div>
+    </ReactMapGl>
   );
 }
