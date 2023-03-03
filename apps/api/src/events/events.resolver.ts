4dee5edab58d6e46ea0cfc5f0d1433955aef117c
Zvonimir Rudinski <zvonimir.rudinski@noubis.com>
2023-03-03 23:22:56 +0100

    feat: add queries for events and mutation for cancelling it

--- apps/api/src/events/events.resolver.ts
+++ apps/api/src/events/events.resolver.ts
@@ -1,4 +1,19 @@
-import { Parent, ResolveField, Resolver } from '@nestjs/graphql';
+import {
+  ForbiddenException,
+  NotFoundException,
+  UseGuards,
+} from '@nestjs/common';
+import {
+  Args,
+  Mutation,
+  Parent,
+  ResolveField,
+  Resolver,
+  Query,
+} from '@nestjs/graphql';
+import { FirebaseGuard } from 'src/common/firebase/firebase.guard';
+import { FirebaseUser } from 'src/common/firebase/firebase.user.decorator';
+import { User } from 'src/user/models/user.model';
 import { Venue } from 'src/venues/models/venue.model';
 import { VenuesService } from 'src/venues/venues.service';
 import { EventsService } from './events.service';
@@ -14,5 +29,32 @@ export class EventsResolver {
   @ResolveField(() => Venue)
   async venue(@Parent() event: Event): Promise<Venue> {
     return this.venuesService.findById(event.venueId);
+  }
+
+  @Query(() => Event)
+  async event(@Args('eventId') eventId: number): Promise<Event> {
+    return this.eventsService.findById(eventId);
+  }
+
+  @Query(() => [Event])
+  async venueEvents(@Args('venueId') venueId: number): Promise<Event[]> {
+    return this.eventsService.findAllByVenueId(venueId);
+  }
+
+  @Mutation(() => Event)
+  @UseGuards(FirebaseGuard)
+  async cancelEvent(
+    @Args('eventId') eventId: number,
+    @FirebaseUser() user: User,
+  ): Promise<Event> {
+    const event = await this.eventsService.findById(eventId);
+    if (!event) {
+      throw new NotFoundException('Event not found');
+    }
+    if (event.venue.firebaseUserId !== user.id) {
+      throw new ForbiddenException('You are not the owner of this event');
+    }
+
+    return this.eventsService.cancelEvent(eventId);
   }
 }
--- apps/api/src/events/events.service.ts
+++ apps/api/src/events/events.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import { EventStatus } from '@prisma/client';
 import { PrismaService } from 'src/common/services/prisma.service';
 import { Event } from './models/event.model';
 
@@ -6,10 +7,25 @@ import { Event } from './models/event.model';
 export class EventsService {
   constructor(private readonly prisma: PrismaService) {}
 
+  async findById(eventId: number): Promise<Event> {
+    return this.prisma.event.findUnique({
+      where: { id: eventId },
+      include: { venue: true },
+    });
+  }
+
   async findAllByVenueId(venueId: number): Promise<Event[]> {
     return this.prisma.event.findMany({
       where: { venueId },
       include: { venue: true },
+    });
+  }
+
+  async cancelEvent(eventId: number): Promise<Event> {
+    return this.prisma.event.update({
+      where: { id: eventId },
+      include: { venue: true },
+      data: { status: EventStatus.CANCELLED },
     });
   }
 }
