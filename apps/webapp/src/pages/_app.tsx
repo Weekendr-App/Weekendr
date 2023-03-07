961e9a312d196c0dc82d9b0e2575e4bb6f7eeee3
Zvonimir Rudinski <zvonimir.rudinski@noubis.com>
2023-03-07 12:55:39 +0100

    feat: add error handling

--- apps/webapp/src/pages/_app.tsx
+++ apps/webapp/src/pages/_app.tsx
@@ -4,6 +4,7 @@ import "@diplomski/styles/globals.css";
 import { UrqlProvider } from "@diplomski/utils/urql";
 import type { AppProps } from "next/app";
 import { lazy } from "react";
+import { Toaster } from "react-hot-toast";
 
 const Header = lazy(() => import("@diplomski/components/Header"));
 
@@ -14,6 +15,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <MapHoverProvider>
           <Header />
           <Component {...pageProps} />
+          <Toaster />
         </MapHoverProvider>
       </UrqlProvider>
     </AuthProvider>
