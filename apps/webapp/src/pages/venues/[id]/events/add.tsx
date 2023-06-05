254f07cb02cc47eaa36101d3ad2042984e4bce36
Yekna <urosjeknic@gmail.com>
2023-06-05 17:15:06 +0200

    Return user to home page if he doesn't own the venue in the add events page

--- apps/webapp/src/pages/venues/[id]/events/add.tsx
+++ apps/webapp/src/pages/venues/[id]/events/add.tsx
@@ -2,14 +2,20 @@ import EventForm, {
   EventFormValues,
 } from "@diplomski/components/Event/EventForm";
 import useAddEvent from "@diplomski/hooks/useAddEvent";
+import useVenue from "@diplomski/hooks/useVenue";
 import Head from "next/head";
 import { useRouter } from "next/router";
 import { useCallback } from "react";
 
 export default function AddEventPage() {
   const { addEvent, result } = useAddEvent();
+  const { venue } = useVenue();
   const router = useRouter();
   const { id } = router.query;
+
+  if (venue?.isOwnedByMe === false) {
+    router.push("/");
+  }
 
   const onSubmit = useCallback(
     async (values: EventFormValues) => {
