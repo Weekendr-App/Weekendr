f198ffe11bfbf7129032eecb82c141a578b7c884
Zvonimir Rudinski <zvonimir.rudinski@noubis.com>
2023-08-09 18:12:30 +0200

    feat: optimize VenueListItem hover

--- apps/webapp/src/components/Venue/VenueListItem.tsx
+++ apps/webapp/src/components/Venue/VenueListItem.tsx
@@ -2,7 +2,7 @@ import { Event, Venue, VenueStatus } from "@diplomski/gql/graphql";
 import clsx from "clsx";
 import Image from "next/image";
 import { useRouter } from "next/router";
-import { FC, useCallback, useEffect, useMemo, useRef, useState } from "react";
+import { FC, useCallback, useMemo } from "react";
 import { useMediaQuery } from "usehooks-ts";
 
 interface Props {
@@ -15,6 +15,7 @@ const VenueListItem: FC<Props> = ({ venue }) => {
   const router = useRouter();
   const isPhone = useMediaQuery("(pointer: coarse)");
   const isSmallScreen = useMediaQuery("(max-width: 640px)");
+  const id = `venue-image-${venue.id}`;
 
   const name = useMemo(() => {
     if (venue.status === VenueStatus.Draft) {
@@ -24,48 +25,36 @@ const VenueListItem: FC<Props> = ({ venue }) => {
     return venue.name;
   }, [venue]);
 
-  const frontRef = useRef<HTMLImageElement | null>(null);
-  const backTextRef = useRef<HTMLParagraphElement | null>(null);
-  const [backTextHeight, setBackTextHeight] = useState<number | null>(null);
-
-  const onHover = useCallback(() => {
-    if (frontRef.current) {
-      frontRef.current.style.transform = `translateY(-${backTextHeight}px)`;
-    }
-  }, [backTextHeight]);
-  const onLeave = useCallback(() => {
-    if (frontRef.current) {
-      frontRef.current.style.transform = `translateY(0)`;
-    }
-  }, []);
-
-  // we use venue inside the dependency array because otherwise this useEffect won't run after we click or tap on a Marker on the map
-  useEffect(() => {
-    if (backTextRef.current) {
-      setBackTextHeight(backTextRef.current.offsetHeight);
-    }
-  }, [venue]);
-
-  // on mobile devices we don't want to have to tap & hold the screen to trigger onMouseEnter so we just run that code automatically if the user is on a mobile device
-  useEffect(() => {
-    isPhone && onHover();
-  }, [onHover, isPhone]);
+  const nameRef = useCallback(
+    (node: HTMLParagraphElement | null) => {
+      if (node !== null) {
+        document.styleSheets[0].insertRule(
+          `#${id}${isPhone ? "" : ":hover"} {
+            transform: translateY(-${node.clientHeight}px);
+          }`,
+          0
+        );
+      } else {
+        document.styleSheets[0].deleteRule(0);
+      }
+    },
+    [id, isPhone]
+  );
 
   return (
     <div
-      className={clsx(["mt-10", "relative"], {
+      className={clsx(["mt-14", "relative"], {
         grayscale: venue.status === VenueStatus.Draft,
         "w-36 h-48": isSmallScreen,
         "w-48 h-64": !isSmallScreen,
       })}
-      onMouseEnter={onHover}
-      onMouseLeave={onLeave}
       onClick={() =>
         venue.status === VenueStatus.Active &&
         router.push(`/venues/${venue.id}`)
       }
     >
       <Image
+        id={id}
         className={clsx(
           ["z-10", "cursor-pointer", "rounded-3xl", "transition-transform"],
           {
@@ -74,7 +63,6 @@ const VenueListItem: FC<Props> = ({ venue }) => {
             "rounded-b-none": isPhone,
           }
         )}
-        ref={frontRef}
         src={venue.picture}
         alt={name}
         fill
@@ -86,7 +74,7 @@ const VenueListItem: FC<Props> = ({ venue }) => {
         })}
       >
         <p
-          ref={backTextRef}
+          ref={nameRef}
           className={clsx(
             [
               "tracking-widest",
@@ -96,10 +84,10 @@ const VenueListItem: FC<Props> = ({ venue }) => {
               "p-5",
               "font-bold",
               "text-gray-800",
+              "text-sm",
             ],
             { "max-w-[192px]": !isSmallScreen, "max-w-[144px]": isSmallScreen }
           )}
-          style={{ fontSize: "0.8em" }}
         >
           {name}
         </p>
